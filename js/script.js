4dcde6e9e66dbe140d553760f56d06539bb529f5
MikkoKarvonen <mkarvonen34@gmail.com>
2019-01-21 16:39:03 +0200

    Add episodes

--- js/script.js
+++ js/script.js
@@ -14,5 +14,40 @@
 });*/
 
 $.each(episodeImages, function (k, v) {
-    $('#episodes').append('<img src='+v+' class="episodeCover">');
+    $('#episodes').append(
+        '<div id="episode'+k+'" class="episodeContainer">'+
+            '<div class="coverContainer" onclick="displayControls('+k+')">'+
+                '<img src='+v+' class="episodeCover">'+
+                '<div class="coverHover">'+
+                    '<h2>'+episodes[k].title.split(' - ')[1]+'</h2>'+
+                '</div>'+
+            '</div>'+
+            '<div class="episodeInfo"></div>'+
+        '</div>');
 });
+
+function displayControls(episodeNum){
+    console.log(episodeNum);
+    if ($('#episode'+episodeNum).hasClass('episodeExpanded')){
+        $('#episode'+episodeNum+' .episodeInfo').empty();
+        $('#episode'+episodeNum).removeClass('episodeExpanded');
+    } else {
+        $('.episodeExpanded .episodeInfo').empty();
+        $('.episodeExpanded').removeClass('episodeExpanded');
+        $('#episode'+episodeNum).addClass('episodeExpanded');
+        $('#episode'+episodeNum+' .episodeInfo').append(
+            '<h2>' + episodes[episodeNum].title.split(' - ')[1] + '</h2>'+
+            '<iframe width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+episodes[episodeNum].guid+'&color=%23ff5500&inverse=true&auto_play=false&show_user=true"></iframe>' +
+            '<p>' + episodes[episodeNum].description + '</p>' 
+        );
+    }
+}
+
+$('.coverContainer').hover(
+    function(){
+       $(this).find('.coverHover').fadeIn(200); 
+    },
+    function(){
+       $(this).find('.coverHover').fadeOut(200); 
+    }
+);
